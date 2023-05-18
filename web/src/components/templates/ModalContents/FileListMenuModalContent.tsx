edc12df38c78698b16c7e62c0db07256dd896185
atsumarukun <atys.smz@gmail.com>
2023-05-18 10:35:08 +0900

    Creation of a drag-and-drop file upload form in FileListMenuModal.

--- web/src/components/templates/ModalContents/FileListMenuModalContent.tsx
+++ web/src/components/templates/ModalContents/FileListMenuModalContent.tsx
@@ -1,19 +1,27 @@
 import { FileSelectStatus } from "@/constants/status";
 import { StorageContext } from "@/providers/storageProvider";
 import {
+  Box,
   Button,
+  Circle,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
+  Stack,
   Text,
   VStack,
   useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useContext } from "react";
-import { AiOutlineCopy, AiOutlineDownload } from "react-icons/ai";
+import { useCallback, useContext } from "react";
+import { useDropzone } from "react-dropzone";
+import {
+  AiOutlineCopy,
+  AiOutlineDownload,
+  AiOutlineUpload,
+} from "react-icons/ai";
 import { CgTrashEmpty } from "react-icons/cg";
 import { MdOutlineDriveFileMove } from "react-icons/md";
 
@@ -102,6 +110,38 @@ export default function FileListMenuModalContent({
     onClose();
   };
 
+  const onDrop = useCallback(async (files: File[]) => {
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    const res = await axios.post(
+      `/api/storage/upload?path=${router.query.path ?? "/"}`,
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    if (res.status === 200) {
+      toast({
+        title: "アップロードしました.",
+        status: "success",
+        duration: 200,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "エラーが発生しました.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    router.reload();
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    multiple: true,
+  });
+
   return (
     <ModalContent mx="5">
       <ModalHeader>FileMenu</ModalHeader>
@@ -153,12 +193,30 @@ export default function FileListMenuModalContent({
             </Button>
           </VStack>
         ) : (
-          <>
-            <Text>ファイルを選択してください.</Text>
-            <Button variant="ghost" float="right" onClick={onClose}>
-              <Text fontWeight="400">閉じる</Text>
-            </Button>
-          </>
+          <Box
+            {...getRootProps()}
+            h="50vh"
+            textAlign="center"
+            position="relative"
+          >
+            <Box
+              position="absolute"
+              w="100%"
+              top="50%"
+              transform="translateY(-50%)"
+            >
+              <Circle size="24" bgColor="gray.100" mx="auto">
+                <AiOutlineUpload size="28" />
+              </Circle>
+              <Text fontSize="12" mt="5">
+                アップロードするファイルをドラッグ＆ドロップします
+              </Text>
+              <Button colorScheme="blue" borderRadius="3" h="8" mt="5">
+                <Text fontSize="12">ファイルを選択</Text>
+              </Button>
+            </Box>
+            <input {...getInputProps()} />
+          </Box>
         )}
       </ModalBody>
     </ModalContent>
