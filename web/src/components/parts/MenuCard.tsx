a3bb006eb1ff92ca727ddb05f1cc57fbf9bc1c02
atsumarukun <atys.smz@gmail.com>
2023-06-12 10:43:28 +0900

    Add service in MenuCard

--- web/src/components/parts/MenuCard.tsx
+++ web/src/components/parts/MenuCard.tsx
@@ -1,14 +1,23 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { HStack, Text, VStack } from "@chakra-ui/react";
 import Link from "next/link";
+import { IoIosApps } from "react-icons/io";
 import { MdStorage } from "react-icons/md";
 
 export default function MenuCard() {
   return (
-    <Link href="/storage">
-      <VStack>
-        <MdStorage size="75" />
-        <Text>Storage</Text>
-      </VStack>
-    </Link>
+    <HStack justifyContent="center" gap="5">
+      <Link href="/storage">
+        <VStack>
+          <MdStorage size="75" />
+          <Text>Storage</Text>
+        </VStack>
+      </Link>
+      <Link href="/service">
+        <VStack>
+          <IoIosApps size="75" />
+          <Text>Sservice</Text>
+        </VStack>
+      </Link>
+    </HStack>
   );
 }
